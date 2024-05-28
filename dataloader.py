3aa078af6bec9578302e2897173d2f423b468468
Hieu Luu <hhluu@ucsd.edu>
2024-05-28 00:49:36 -0700

    vertices generation

--- dataloader.py
+++ dataloader.py
@@ -1,10 +1,12 @@
 import torch
 from torchvision.datasets import CocoDetection
 import numpy as np
+import json
 
 class LineMODCocoDataset(CocoDetection):
-    def __init__(self, root, annFile, transform=None, target_transform=None):
+    def __init__(self, root, annFile, modelsPath, transform=None, target_transform=None):
         super(LineMODCocoDataset, self).__init__(root, annFile, transform, target_transform)
+        self.models = json.load(open(modelsPath, 'r'))
 
     def apply_augmentations(self, image):
         # Add Gaussian noise
@@ -20,10 +22,40 @@ class LineMODCocoDataset(CocoDetection):
         image = np.clip(image, 0, 255).astype(np.uint8)
         return image
 
-    def generate_ground_truth(self, image, pose):
+    def create_3D_vertices(self, model):
+        vertices = np.empty((3, 0))
+        x, y, z = model['min_x'], model['min_y'], model['min_z']
+        size_x, size_y, size_z = np.array([model['size_x'], 0, 0]).reshape(3, -1),\
+                                 np.array([0, model['size_y'], 0]).reshape(3, -1),\
+                                 np.array([0, 0, model['size_z']]).reshape(3, -1)
+        v1 = np.array([x, y, z]).reshape(3, -1)
+        v2 = v1 + size_x
+        v3 = v1 + size_y
+        v4 = v1 + size_z
+        v5 = v1 + size_x + size_y
+        v6 = v1 + size_x + size_z
+        v7 = v1 + size_y + size_z
+        v8 = v1 + size_x + size_y + size_z
+        vertices = np.concatenate([eval(f'v{i}') for i in np.arange(1, 9)], axis=1)
+        return vertices
+
+    def project_3D_vertices(self, vertices, target):
+        pose = self.extract_pose(target) # 3 x 4
+        P_m2c = np.array(target['cam_K']).reshape(3, 3) @ pose
+        vertices = np.vstack((vertices, np.zeros(vertices.shape[1])))
+        homogeneous_2d = P_m2c @ vertices
+        pixel_coordinates = homogeneous_2d[:2, :] / homogeneous_2d[2, :]
+        return pixel_coordinates
+
+    def generate_ground_truth(self, image, target):
         h, w = image.shape[:2]
         belief_map = np.zeros((9, h, w), dtype=np.float32)
         vector_field = np.zeros((16, h, w), dtype=np.float32)
+        cat = target['category_id']
+        model = self.models[cat]
+        vertices = self.create_vertices(model)
+        projected_vertices = self.project_3D_vertices(vertices, target)
+        belief_map = 
 
         # Implement the actual logic for generating belief maps and vector fields
         # using 2D Gaussians and normalized vectors as described in the provided info.
@@ -31,21 +63,16 @@ class LineMODCocoDataset(CocoDetection):
         return belief_map, vector_field
 
     def __getitem__(self, index):
-        img, target = super(LineMODCocoDataset, self).__getitem__(index)
+        out = super(LineMODCocoDataset, self).__getitem__(index)
+        img, target = out[0], out[1][0]
         img = np.array(img)
-        # Assume the target contains pose information
-        pose = self.extract_pose(target) # 3 x 4
-
-        P_m2c = target['cam_K'].reshape(3, 3) @ pose
-
-        
 
         if self.transform:
             img = self.transform(img)
         else:
             img = self.apply_augmentations(img)
 
-        belief_map, vector_field = self.generate_ground_truth(img, pose)
+        belief_map, vector_field = self.generate_ground_truth(img, target)
         gt_maps = np.concatenate((belief_map, vector_field), axis=0)
 
         img = torch.from_numpy(img.transpose((2, 0, 1))).float() / 255.0
@@ -56,8 +83,8 @@ class LineMODCocoDataset(CocoDetection):
     def extract_pose(self, target):
         # Placeholder for extracting pose information from the target
         # This should be implemented according to the specifics of your dataset
-        R, t = target['cam_R_m2c'], target['cam_t_m2c']
-        pose = np.concatenate([R.reshape(3, 3), t])
+        R, t = np.array(target['cam_R_m2c']), np.array(target['cam_t_m2c'])
+        pose = np.hstack([R.reshape(3, 3), t.reshape(3, -1)])
         return pose
 
 if __name__ == '__main__':
@@ -66,7 +93,7 @@ if __name__ == '__main__':
 
     # Paths to your dataset
     root = '/path/to/linemod/test_data'
-    annFile = '/path/to/linemod/annotations.json' # TODO wtf is this
+    annFile = '/path/to/linemod/annotations.json' 
 
     dataset = LineMODCocoDataset(root, annFile, transform=ToTensor())
 
